684028206392227c098f2c2f011eaeac66cecde8
Abhiith S <abhijithpgd13@gmail.com>
2026-01-13 23:52:27 +0530

    added comment idempotency

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -15,9 +15,19 @@ const commentSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         required: true,
         ref: 'User'
+    },
+    idempotencyKey: {
+        type: String,
+        required: true,
     }
 },
   {timestamps : true}
 )
+
+commentSchema.index(
+  { author: 1, post: 1, idempotencyKey: 1 },
+  { unique: true }
+);
+
 
 module.exports = mongoose.model("Comment" , commentSchema)
